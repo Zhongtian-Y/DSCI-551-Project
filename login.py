332f63133290cb6db43718f6e04fbca54da9f6f2
Zhongtian-Y <143072950+Zhongtian-Y@users.noreply.github.com>
2024-03-01 05:28:44 -0800

    v1 for midterm report

---
+++ login.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_mysqldb import MySQL
+from werkzeug.security import check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'dsci551'
+
+# MySQL config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'dsci551'
+app.config['MYSQL_DB'] = 'users_info'
+
+mysql = MySQL(app)
+
+# Login process starts
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin):
+    def __init__(self, id, username, type):
+        self.id = id
+        self.username = username
+        self.type = type
+
+# connect to the database where users_info stored
+@login_manager.user_loader
+def load_user(user_id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id, username, type FROM users WHERE id = %s", (user_id,))
+    user = cur.fetchone()
+    cur.close()
+    if user:
+        return User(id=user[0], username=user[1], type=user[2])
+    return None
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        type = request.form['type']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT id, hashed_password, type FROM users WHERE username = %s", (username,))
+        user = cur.fetchone()
+        cur.close()
+
+        # redirect to the dashboard if username and password is in the database, with username, type and password matching
+        if user and check_password_hash(user[1], password) and user[2] == type:
+            user_obj = User(id=user[0], username=username, type=user[2])
+            login_user(user_obj)
+            return redirect(url_for('dashboard'))
+        
+        # return the error message if user type does not match other information
+        if user and check_password_hash(user[1], password) and not user[2] == type:
+            return 'User type does not match'
+        
+        # return the error message if password and username are not correct
+        else:
+            return 'Invalid username or password'
+        
+    return render_template('login.html')
+
+# render the dashboard if a user/admin successfully logged in
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+# log out from the dashboard if the user/admin click the Log out button
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ users_info.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
+
+app = Flask(__name__)
+
+# MySQL config
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'dsci551'
+app.config['MYSQL_DB'] = 'users_info'
+
+mysql = MySQL(app)
+
+# Insert user info starts
+def insert_multiple_users(users_data):
+    with app.app_context():
+
+    # Hash the password
+        users_to_insert = [
+            (users["id"], users["username"], generate_password_hash(users["password"]), users["type"]) for users in users_data]
+    
+    # Insert new user and check if it is successful
+        query = "INSERT INTO users (id, username, hashed_password, type) VALUES (%s, %s, %s, %s)"
+        cur = None
+        try:
+            cur = mysql.connection.cursor()
+            cur.executemany(query, users_to_insert)
+            mysql.connection.commit()
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            if cur: 
+                cur.close()
+
+if __name__ == "__main__":
+    users_data = [
+    {"id": 1, "username": "user1", "password": "password1", "type": "administrator"},
+    {"id": 2, "username": "user2", "password": "password2", "type": "administrator"},
+    {"id": 3, "username": "user3", "password": "password3", "type": "administrator"},
+    {"id": 4, "username": "user4", "password": "password4", "type": "administrator"},
+    {"id": 5, "username": "user5", "password": "password5", "type": "administrator"},
+    {"id": 6, "username": "user6", "password": "password6", "type": "normal"},
+    {"id": 7, "username": "user7", "password": "password7", "type": "normal"},
+    {"id": 8, "username": "user8", "password": "password8", "type": "normal"},
+    {"id": 9, "username": "user9", "password": "password9", "type": "normal"},
+    {"id": 10, "username": "user10", "password": "password10", "type": "normal"},
+]
+    # Run the script
+    insert_multiple_users(users_data)
+    print("Users inserted successfully.")
