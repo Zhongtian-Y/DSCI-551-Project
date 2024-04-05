5bf745b4d631dcd12ebb6d3a94063fc302d10492
Zhongtian-Y <143072950+Zhongtian-Y@users.noreply.github.com>
2024-04-05 03:19:56 -0700

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,3 +1,3 @@
 DROP TABLE IF EXISTS users;
-CREATE TABLE users(id int, username varchar(20), hashed_password varchar(255), type varchar(20));userstest
+CREATE TABLE users(id int, username varchar(20), hashed_password varchar(255), type varchar(20));
 SELECT * FROM usercrud.users;
